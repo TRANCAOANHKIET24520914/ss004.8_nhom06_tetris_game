658e2b53dfd2d3c0d6ce29ff59a58768dab036e4
24520914 <24520914@gm.uit.edu.vn>
2025-12-28 11:48:18 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,4 @@
-#include "Game.h"
-#include "InputHandler.h"
-#include <windows.h>
 
-int main() {
-    system("cls");
-    SetConsoleOutputCP(437);
-
-    Game game;
-    InputHandler input;
-
-    while (true) {
-        input.process(game);
-        game.update();
-        Sleep(10);
-    }
-
-    return 0;
-}
 #include "Game.h"
 #include "InputHandler.h"
 #include <windows.h>
